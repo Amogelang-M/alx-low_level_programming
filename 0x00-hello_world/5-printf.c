50a0ef0e84272d684481506993c17b71e84b0fe4
Amogelang-M <amogelangmogane@gmail.com>
2023-10-09 17:08:58 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - a program that prints in line using prinf
+ * return: 0
+ * */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	Return (0);
+}
